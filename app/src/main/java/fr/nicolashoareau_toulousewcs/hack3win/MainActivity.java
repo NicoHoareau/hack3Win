91957e2874a79693085044f4c22241dd77086429
NicoHoareau <nicolas.hoareau.pro@orange.fr>
2018-06-14 20:35:57 +0200

    nouveau dev nico1

--- app/src/main/java/fr/nicolashoareau_toulousewcs/hack3win/MainActivity.java
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/hack3win/MainActivity.java
@@ -2,11 +2,16 @@ package fr.nicolashoareau_toulousewcs.hack3win;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -21,22 +26,61 @@ import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 import java.util.ArrayList;
 
 
-public class MainActivity extends YouTubeBaseActivity {
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
 
 
-    YouTubePlayerView playerView;
-    private final String API_KEY="AIzaSyBt5qNEPgi0aIJrgMzE_JkH9uVbgh8Vy7U";
-    private final String VIDEO_CODE="nc7Z3EBMevg";
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Navigation Drawer :
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_menu);
+        mToggle = new ActionBarDrawerToggle(MainActivity.this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Navigation View :
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+    }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.profil) {
+            //Intent goToProfil = new Intent(ZoomArticlesActivity.this, ProfilActivity.class);
+            //this.startActivity(goToProfil);
+        } else if (id == R.id.reader) {
+            //Intent goToJoin = new Intent(this, JoinQuizzActivity.class);
+            //this.startActivity(goToJoin);
+        } else if (id == R.id.contributor) {
+            Intent goToContributor = new Intent(this, ContributeurActivity.class);
+            this.startActivity(goToContributor);
+        } else if (id == R.id.articles) {
+            //intent
 
+        } else if (id == R.id.logout) {
+            //Déconnexion
+            /*mAuth = FirebaseAuth.getInstance();
+            mAuth.signOut();
+            startActivity(new Intent(this, MainActivity.class));*/
+        }
+        return true;
 
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
