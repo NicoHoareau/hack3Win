38b13a9d1a29c4c8f01ff7a2d83a4f8b9a42fd3d
Valentin Destruel <destruel.valentin@gmail.com>
2018-06-14 10:37:11 +0200

    Firebase

--- app/src/main/java/fr/nicolashoareau_toulousewcs/hack3win/ConnectionActivity.java
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/hack3win/ConnectionActivity.java
@@ -8,12 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.nio.charset.Charset;
 
 
 public class ConnectionActivity extends AppCompatActivity {
 
     private int mSpinnerPosition;
+    private String mStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +34,40 @@ public class ConnectionActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        Button buttonConnection = findViewById(R.id.bt_connection);
+        final Button buttonConnection = findViewById(R.id.bt_connection);
+        final EditText editTextNumber = findViewById(R.id.et_number);
+        final EditText editTextPassword = findViewById(R.id.et_password);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference();
 
         buttonConnection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 mSpinnerPosition = spinner.getSelectedItemPosition();
+                final String stringNumber = editTextNumber.getText().toString();
+                final String stringPassword = editTextPassword.getText().toString();
+                final HashCode hashCode = Hashing.sha256().hashString(stringPassword, Charset.defaultCharset());
 
                 if (mSpinnerPosition == 0) {
+
+                    mStatus = "Lecteur";
+
+                    UserModel studentModel = new UserModel(stringNumber, hashCode.toString(), mStatus, null, null, null);
+                    DatabaseReference studentRef = myRef.child("Users");
+                    studentRef.push().setValue(studentModel);
 
                     Intent intentMainActivity = new Intent(ConnectionActivity.this, MainActivity.class);
                     startActivity(intentMainActivity);
                 } else {
 
+                    mStatus = "Contributeur";
+
                     AlertDialog.Builder builder = new AlertDialog.Builder(ConnectionActivity.this);
 
                     LayoutInflater inflater = getLayoutInflater();
-                    View dialogView = inflater.inflate(R.layout.alertdialog_custom_view, null);
+                    final View dialogView = inflater.inflate(R.layout.alertdialog_custom_view, null);
 
                     builder.setCancelable(false);
 
@@ -55,6 +80,18 @@ public class ConnectionActivity extends AppCompatActivity {
                     btn_positive.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
+
+                            final EditText editTextName = dialogView.findViewById(R.id.et_name);
+                            final EditText editTextFirstName = dialogView.findViewById(R.id.et_firstname);
+                            final EditText editTextEmployer = dialogView.findViewById(R.id.et_employer);
+
+                            final String stringName = editTextName.getText().toString();
+                            final String stringFirstName = editTextFirstName.getText().toString();
+                            final String stringEmployer = editTextEmployer.getText().toString();
+
+                            UserModel studentModel = new UserModel(stringNumber, hashCode.toString(), mStatus, stringName, stringFirstName, stringEmployer);
+                            DatabaseReference studentRef = myRef.child("Users");
+                            studentRef.push().setValue(studentModel);
 
                             dialog.cancel();
 
